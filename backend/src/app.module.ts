ec51ff08df39b72bfb4bc46d4b22f7a2bc7c90b4
gonzoibox <gonzoibox@gmail.com>
2020-10-14 15:33:04 +0300

    add /front

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoModule } from './modules/todo/todo.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), TodoModule],
+  imports: [
+    TypeOrmModule.forRoot(), 
+    TodoModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'front'),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
